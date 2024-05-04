f874b0642a72d3de407c44027cc61c1182d132b8
Guilherme M. Miranda <alchemist.software@proton.me>
2024-05-04 16:37:06 -0300

    define data files

--- build.py
+++ build.py
@@ -1,7 +1,51 @@
 #!/usr/bin/env python3
 
-def main():
-    return 0
+VERSION = '0.1'
 
-if __name__ == "__main__":
-    main()
+DIR_DATA = 'data/'
+DIR_SOURCE = 'source_wads/'
+DIR_DEST = 'build/'
+
+MASTER_LEVELS_WADS = []
+
+MASTER_LEVELS_WADS_LUMPS = {
+	# Ultimate Doom
+	'DOOM.WAD'    : 'doom.txt',
+	# Final Doom, TNT: Evilution
+	'TNT.WAD'     : 'tnt.txt',
+	# Inferno
+	'VIRGIL.WAD'  : 'virgil.wad',
+	'MINOS.WAD'   : 'minos.txt',
+	'NESSUS.WAD'  : 'nessus.txt',
+	'GERYON.WAD'  : 'geryon.txt',
+	'VESPERAS.WAD': 'vesperas.txt',
+	# Titan
+	'MANOR.WAD'   : 'manor.txt',
+	'TTRAP.WAD'   : 'ttrap.txt',
+	# Cabal
+	'BLOODSEA.WAD': 'bloodsea.txt',
+	'BLACKTWR.WAD': 'blacktwr.txt',
+	'MEPHISTO.WAD': 'mephisto.txt',
+	'TEETH.WAD'   : 'teeth.txt',
+	# Klietech
+	'SUBSPACE.WAD': 'subspace.txt',
+	'COMBINE.WAD' : 'combine.txt',
+	'FISTULA.WAD' : 'fistula.txt',
+	'SUBTERRA.WAD': 'subterra.txt',
+	'CATWALK.WAD' : 'catwalk.txt',
+	'GARRISON.WAD': 'garrison.txt',
+	# Lost Levels
+	'ATTACK.WAD': 'attack.txt',
+	'CANYON.WAD': 'canyon.txt',
+}
+
+def main() -> None :
+	raise NotImplementedError(
+	"""
+	>:(
+	Not implemented yet.
+	Get to writing, lazy ass."""
+	)
+
+if __name__ == '__main__':
+	main()
