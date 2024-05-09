b6307811297ed461c1d0d2ad519a2992c714cc75
Guilherme M. Miranda <alchemist.software@proton.me>
2024-05-09 20:25:44 -0300

    added basic fixing of titan maps textures

--- src/base.py
+++ src/base.py
@@ -9,6 +9,7 @@ from os import listdir
 from hashlib import sha256
 
 from omg.wad import WAD
+from omg.mapedit import MapEditor
 
 VERSION = '0.3-proto'
 
@@ -34,10 +35,10 @@ SOURCE_SHA256SUM = {
     # Alpha
     'alpha.wad': '732c51c4a16dcbd3fa59ba9b28217ffee431b311813be21f4cb1a66dbcd431ef',
     # Titan
-    'MINES.WAD': 'af38046cd3523f43837d9d19fbb9e2fac7d6b5799822f33e0963cd5ca14d0d0b',
-    'anomaly.wad': 'cc843c3604ae854acc7b2724704498a22fbb000a0a2dda05215e666bae7424eb',
-    'FARSIDE.WAD': '5d2e937c470c5143e4f69069e372515e483ddc5c1b30742b03fc3dfb63bbd2a3',
-    'TROUBLE.WAD': '2af8071d14718687cec3d03518b9bccbb1e14dc7a17f71321d2979ddd6df7e23',
+    'MINES.WAD': '4156ed584a667f3d97cd4e3795e0cafa7194404855d9c88826e6e417da9e5d5d',
+    'anomaly.wad': 'e0d3efc52add92974cf989c34ce93dbb35149041a1192a2ea169e24922490dad',
+    'FARSIDE.WAD': '55e40715b49c201ea035eef00b5ece40243ca217a56b20c1fb7625c7103ac277',
+    'TROUBLE.WAD': '8f89694bdaeb10709acea88423929bf4ea75cfc8f6d406565151076ccbc365f5',
 }
 
 MAP_TRIPLETS = [
@@ -60,6 +61,7 @@ BASE_MAPS = [
 # Utils
 #
 
+
 def log(line: str) -> None:
     global logfile
 
@@ -81,6 +83,7 @@ def get_wad_filename(wad_name: str) -> str | None:
 #
 # Check WAD data
 #
+
 
 def get_wad_pre_hash(wad_name: str) -> str | None:
     if wad_name not in SOURCE_SHA256SUM:
@@ -118,8 +121,20 @@ def check_wads(found_wads: list[str]):
 # Actual lump-to-wad extraction
 #
 
+
+def massive_simple_sidedef_switch(map: MapEditor, initial_tx: str, desired_tx: str) -> None:
+    for sidedef in map.sidedefs:
+        if sidedef.tx_up == initial_tx:
+            sidedef.tx_up = desired_tx
+        if sidedef.tx_mid == initial_tx:
+            sidedef.tx_mid = desired_tx
+        if sidedef.tx_low == initial_tx:
+            sidedef.tx_low = desired_tx
+
+
 def base_build() -> None:
     base = WAD()
+    alpha = WAD('alpha.wad')
 
     if get_wad_hash(ALPHA) != get_wad_pre_hash(ALPHA):
         log('  Error: alpha.wad failed the checksum, something terrible happened. Try again.')
@@ -128,13 +143,61 @@ def base_build() -> None:
 
     log('  Extracting maps...')
     for triple in MAP_TRIPLETS:
-        base.maps[triple[0]] = WAD(DIR_SOURCE + triple[1]).maps[triple[2]]
+        alpha.maps[triple[0]] = WAD(DIR_SOURCE + triple[1]).maps[triple[2]]
+
+    log(' Fixing MAP10...')
+    map10 = MapEditor(alpha.maps['MAP10'])
+    massive_simple_sidedef_switch(map10, 'DBRAIN1', 'WATER')
+    massive_simple_sidedef_switch(map10, 'SW1COMP', 'TT1COMP')
+    massive_simple_sidedef_switch(map10, 'SW2COMP', 'TT2COMP')
+    massive_simple_sidedef_switch(map10, 'SW1STON1', 'TT1STON1')
+    massive_simple_sidedef_switch(map10, 'SW2STON1', 'TT2STON1')
+    alpha.maps['MAP10'] = map10.to_lumps()
+
+    log(' Fixing MAP11...')
+    map11 = MapEditor(alpha.maps['MAP11'])
+    massive_simple_sidedef_switch(map11, 'SW1STON2', 'TT1STON2')
+    massive_simple_sidedef_switch(map11, 'SW2STON2', 'TT2STON2')
+    alpha.maps['MAP11'] = map11.to_lumps()
+
+    log(' Fixing MAP12...')
+    map12 = MapEditor(alpha.maps['MAP12'])
+    massive_simple_sidedef_switch(map12, 'SW1BRIK', 'TT1BRIK')
+    massive_simple_sidedef_switch(map12, 'SW2BRIK', 'TT2BRIK')
+    massive_simple_sidedef_switch(map12, 'SW1STON3', 'TT1STON3')
+    massive_simple_sidedef_switch(map12, 'SW2STON3', 'TT2STON3')
+    massive_simple_sidedef_switch(map12, 'SW1STON4', 'TT1STON4')
+    massive_simple_sidedef_switch(map12, 'SW2STON4', 'TT2STON4')
+    alpha.maps['MAP12'] = map12.to_lumps()
+
+    log(' Fixing MAP15...')
+    map15 = MapEditor(alpha.maps['MAP15'])
+    massive_simple_sidedef_switch(map15, 'SW1VINE', 'TT1VINE')
+    massive_simple_sidedef_switch(map15, 'SW2VINE', 'TT2VINE')
+    massive_simple_sidedef_switch(map15, 'SW1PIPE', 'TT1PIPE')
+    massive_simple_sidedef_switch(map15, 'SW2PIPE', 'TT2PIPE')
+    massive_simple_sidedef_switch(map15, 'SW1STON5', 'TT1STON5')
+    massive_simple_sidedef_switch(map15, 'SW2STON5', 'TT2STON5')
+    massive_simple_sidedef_switch(map15, 'SW1STON6', 'TT1STON6')
+    massive_simple_sidedef_switch(map15, 'SW2STON6', 'TT2STON6')
+    massive_simple_sidedef_switch(map15, 'SW1STON7', 'TT1STON7')
+    massive_simple_sidedef_switch(map15, 'SW2STON7', 'TT2STON7')
+    alpha.maps['MAP15'] = map15.to_lumps()
+
+    log('  Organizing lumps...')
+    base.data += alpha.data
+    base.music += alpha.music
+    base.txdefs += alpha.txdefs
+    base.patches += alpha.patches
+    base.graphics += alpha.graphics
+    for map in BASE_MAPS:
+        base.maps[map] = alpha.maps[map]
 
     log('  Creating base.wad...')
     base.to_file(BASE)
 
-    if get_wad_hash(BASE) != get_wad_pre_hash(BASE):
-        log('  Warn: base.wad failed the checksum, you can _maybe_ build masterpack.wad.')
+    # if get_wad_hash(BASE) != get_wad_pre_hash(BASE):
+    #    log('  Warn: base.wad failed the checksum, you can _maybe_ build masterpack.wad.')
 
 
 def base_fix() -> None:
