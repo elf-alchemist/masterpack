665075c7650a7265d8090d4bda3f664e51c05a09
Guilherme M. Miranda <alchemist.software@proton.me>
2024-05-09 04:04:50 -0300

    add the world's simplest build tool

---
+++ masterpack.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: Copyright 2024 Guilherme M. Miranda <alchemist.software@proton.me>
+#
+# Description:
+#     Simplest build script possible
+
+# Bash strict mode - Early exit, instead of failing later
+set -o errtrace
+set -o errexit
+set -o nounset
+set -o pipefail
+
+FILE=".github/masterpack.zip"
+
+if [[ -e "${FILE}" ]]; then rm "${FILE}"; fi
+
+zip "${FILE}" -u omg/* source/delete_me.txt base.wad masterpack.py
