bb89cba1059fec7e9cf176024928eade515e80e5
Guilherme M. Miranda <alchemist.software@proton.me>
2024-05-12 15:09:57 -0300

    preparing for massive cahnges
    moving code from masterpack.py to base.py
    with the objective of achieving complete automation

--- src/base.py
+++ src/base.py
@@ -3,11 +3,11 @@
 # SPDX-FileCopyrightText: Copyright 2024 Guilherme M. Miranda <alchemist.software@proton.me>
 #
 # Description:
-#     Masterpack base builder, a utility for creating the base.wad file needed for building masterpack.wad
+#     Masterpack build script. Bring together all Master Levels for Doom II in one single package.
 
 from os import listdir
 from hashlib import sha256
-from pprint import pprint
+from zipfile import ZipFile
 
 from omg.wad import WAD
 from omg.mapedit import MapEditor
@@ -17,54 +17,83 @@ LOG_FILE = 'base.log'
 
 BUFFER_SIZE = 16384
 
-DIR_SOURCE = 'base/'
+DIR_SOURCE = 'source/'
 
 ALPHA = 'alpha.wad'
-BASE = 'beta.wad'
+BASE = 'base.wad'
+MASTERPACK = 'masterpack.wad'
 
-SOURCE_WADS = [
-    'DANTE25.WAD', 'ACHRON22.WAD', 'UDTWiD.wad',
-    'MINES.WAD', 'anomaly.wad', 'FARSIDE.WAD', 'TROUBLE.WAD',
-    'CABALL.WAD',
-    'cpu.wad', 'device_1.wad', 'dmz.wad', 'cdk_fury.wad', 'e_inside.wad', 'hive.wad',
+ALL_WADS = [
+    'DOOM.WAD', 'TNT.WAD',
+    'DANTE25.WAD', 'ACHRON22.WAD', 'VIRGIL.WAD', 'MINOS.WAD', 'NESSUS.WAD', 'GERYON.WAD', 'VESPERAS.WAD', 'UDTWiD.wad',
+    'MINES.WAD', 'anomaly.wad', 'FARSIDE.WAD', 'MANOR.WAD', 'TTRAP.WAD', 'TROUBLE.WAD',
+    'CABALL.WAD', 'BLOODSEA.WAD', 'BLACKTWR.WAD', 'MEPHISTO.WAD', 'TEETH.WAD',
+    'cpu.wad', 'SUBSPACE.WAD', 'COMBINE.WAD', 'device_1.wad', 'FISTULA.WAD', 'SUBTERRA.WAD', 'dmz.wad', 'CATWALK.WAD', 'cdk_fury.wad', 'GARRISON.WAD', 'e_inside.wad', 'hive.wad',
+    'TWM01.WAD', 'PARADOX.WAD', 'ATTACK.WAD', 'CANYON.WAD', 'kickdm2.wad'
     'ultimidi.wad', 'midtwid2.wad',
 ]
 
-SOURCE_SHA256SUM = {
+SHA256_DIGEST = {
     'alpha.wad': 'a51d69955c4a0141fb353f6732cf4421ea275f641789e10907a0169b34915234',
+    'base.wad': 'cc1381363256199b5e93031c7d84cc7d27ed1c23db83f8421d46e4ac9ed16181',
+    'masterpack.wad': '0dfe970900481a6b474c51e5ceabed53754b3163674dcd9bdcd91f3cdd9df030',
+
+    'DOOM.WAD': '6fdf361847b46228cfebd9f3af09cd844282ac75f3edbb61ca4cb27103ce2e7f',
+    'TNT.WAD': 'c0a9c29d023af2737953663d0e03177d9b7b7b64146c158dcc2a07f9ec18f353',
 
     'DANTE25.WAD': '8ac0ce535c75027f46a8909ad6fa7c173ba1457a76167a05ac2213d5606cf902',
     'ACHRON22.WAD': '9c285f5b14b5a26a5e46e0c5566a1fbfc94a170a9d0d6c2dee19069b3e4d5423',
+    'VIRGIL.WAD': 'c468a1684be8e8055fca52c0c0b3068893481dbaeff0d25f2d72a13b340dff09',
+    'MINOS.WAD': 'fc3996e52b527dd4d7e76b023eebaa0c18263c94e21115b06ba64c8cda371ec0',
+    'NESSUS.WAD': '05a2e5f7e13acfe21895f6ad2ecd216052d8c5c4bcb65b67745678ca0ae3e0dc',
+    'GERYON.WAD': 'ec5c0b1b846764d4b7cc8038e5fac1b1562627d0bcca413a55e918d3141cbfbc',
+    'VESPERAS.WAD': '14a515f480455a4bd23c8204d9d236c9ba18c147e8c10a364300832ee804fea3',
     'UDTWiD.wad': '523108f0341da424fc390c2a43cb3a9d418f0137298e85b19d12272da4021ec7',
 
     'MINES.WAD': '4156ed584a667f3d97cd4e3795e0cafa7194404855d9c88826e6e417da9e5d5d',
     'anomaly.wad': 'e0d3efc52add92974cf989c34ce93dbb35149041a1192a2ea169e24922490dad',
     'FARSIDE.WAD': '55e40715b49c201ea035eef00b5ece40243ca217a56b20c1fb7625c7103ac277',
+    'MANOR.WAD': 'fe0a502310ef1b32597e2e82d6f7f12ad77cb603ac5838056dee86f4bae31e2f',
+    'TTRAP.WAD': '90c7d8fc103e7b02b9602d5eb37fa439da1c5880355e4ee9fa53428ef30dcf45',
     'TROUBLE.WAD': '8f89694bdaeb10709acea88423929bf4ea75cfc8f6d406565151076ccbc365f5',
 
     'CABALL.WAD': '5ed07c63382124793eb8c2881a4d85da15d430d32265c14d8452930ef3f2c2c4',
+    'BLOODSEA.WAD': '1b1acb09f4319db32903dc502d84fc034d21611e375771f4aaac66fafabea39a',
+    'BLACKTWR.WAD': 'a0120667412fcf293ea13cdc31c1fa0626d7828f83a195aa09f4170797d07473',
+    'MEPHISTO.WAD': '585040c5408bc66583dbe1b8e36d009c81799f7dbebd002af48e4c00088bfbd5',
+    'TEETH.WAD': '629e2a6eb1a0234ef0f152384a01130b3d8cd0e44f6c292463dbc2d6ae34d14e',
 
     'cpu.wad': '9377554a75a18eaa2cd60a8707040efeaa21d136e842b0060d4262972f6b790f',
+    'SUBSPACE.WAD': '2c61963efe338abc38b13fb01f238fd95d6b0e2a627ca78db038b9761ff6e6ac',
+    'COMBINE.WAD': 'fef0ac1090ba2f0bce92503625047d726c2d8d1964561bd557234e7ab6202dff',
     'device_1.wad': 'f1493e01d7f3fdcda098ebf4c48560fe098b9c56610650b44bd535e0255d7d54',
+    'FISTULA.WAD': '864d51febc039b1431c5d75fb051f567433adbddce4bb4d8b6e959fc0b414724',
+    'SUBTERRA.WAD': '3dba2abcb85ae3685c259b21bd867d542439c591c12162a36066f1fbab6cbdd8',
     'dmz.wad': '365e62691813d9653d588c65590b889cf0fb16276363c3d5454fbcabbb45eaea',
+    'CATWALK.WAD': 'ce1241e16b2e8bb8bbceea84a281e81f865c52670e4eb3649dc7372870985b77',
     'cdk_fury.wad': '1cafc59bd422991c93f9c34830793375c9914cf4ffae88db927867d6871888a8',
+    'GARRISON.WAD': '7a7a37d1f31c51ced17167f5283daffef27d79310df0c96b13f4cb1723a2417c',
     'e_inside.wad': 'f5f97aa3f09c97a99f3721aeda32037e55cf4f936f9a74fa8d4c5bcf57ab3eae',
     'hive.wad': '754479211ffcbf240c08c6cdc6c1912f1c2eee3626682d04fe286a8fb55d1d70',
+
+    'TWM01.WAD': '15d81c07374893165971e4ab79af2523729e6acf55688237734f821c771536aa',
+    'PARADOX.WAD': '4e9f37b8209dc7006637050876d917e9a68b053ef24e17725432781b00969bdf',
+    'ATTACK.WAD': '4b9a404a4ee43ed33f7c2e208269b84b58ccfec5823afa1fe50e3dd08e927622',
+    'CANYON.WAD': 'a2dd18d174d25a5d31046114bf73d87e9e13e49e9e6b509b2c9942e77d4c9ecf',
+    'kickdm2.wad': '18902e534468304ee505e6c95755592acf6a91a90672152fcaf2f552d0cee786',
 
     'ultimidi.wad': 'cff289109308dceffb0f2e9361196bda1c35edca7665c5ad604d335ad8d756c5',
     'midtwid2.wad': '54806d8606273cda58db29477fb3fc8be8e40c774f587dda217dab1015e1547b',
 }
 
 PATCH_TRIPLETS = [
-    # patch X, in wad Y, as Z
     ['MSKY1', 'UDTWiD.wad', 'SKY4'],
     ['MSKY2_1', 'MINES.WAD', 'STARS'],
     ['MSKY2_2', 'MINES.WAD', 'STARS1'],
     ['MSKY2_3', 'MINES.WAD', 'STARSAT'],
 
     ['DRSLEEP', 'UDTWiD.wad', 'DRSLEEP'],
-    # ['UN_SKY', 'ACHRON22.WAD', 'SKY1'],
-    # ^ unused because MSKY1 is a MUCH better fit for Inferno
+    # ['UN_SKY', 'ACHRON22.WAD', 'SKY1'], <-- unused because MSKY1 is a MUCH better fit for Inferno
 
     ['ASHWALL', 'MINES.WAD', 'W104_1'],
     ['WATER', 'MINES.WAD', 'TWF'],
@@ -86,47 +115,160 @@ PATCH_TRIPLETS = [
     ['TROUBLE3', 'TROUBLE.WAD', 'TROU06'],
     ['TROUBLE4', 'TROUBLE.WAD', 'TROU07'],
     ['TROUBLE5', 'TROUBLE.WAD', 'TROU13'],
+    ['SW2_3', 'DOOM.WAD', 'SW2_3'],
+    ['W15_6', 'DOOM.WAD', 'W15_6'],
+    ['WALL40_1', 'DOOM.WAD', 'WALL40_1'],
+    ['WALL63_2', 'DOOM.WAD', 'WALL63_2'],
+    ['WALL76_1', 'DOOM.WAD', 'WALL76_1'],
+    ['W109_1', 'DOOM.WAD', 'W109_1'],
+    ['W109_2', 'DOOM.WAD', 'W109_2'],
+    ['W110_1', 'DOOM.WAD', 'W110_1'],
+    ['W113_1', 'DOOM.WAD', 'W113_1'],
+    ['W113_2', 'DOOM.WAD', 'W113_2'],
+    ['W113_3', 'DOOM.WAD', 'W113_3'],
 ]
 
-MAP_TRIPLETS = [
+ALL_PATCHES = [
+    'MSKY1',
 
+    'MSKY2_1',
+    'MSKY2_2',
+    'MSKY2_3',
+    'MSKY3',
+
+    'DRSLEEP',
+
+    'ASHWALL',
+    'WATER',
+    'REDWALL1',
+    'REDWALL2',
+
+    'BLACK',
+    'FIRELV',
+    'ANOMALY1',
+    'ANOMALY2',
+    'ANOMALY3',
+    'ANOMALY4',
+    'ANOMALY5',
+    'ANOMALY6',
+    'ANOMALY7',
+    'ANOMALY8',
+
+    'SAVED',
+    'TROUBLE1',
+    'TROUBLE2',
+    'TROUBLE3',
+    'TROUBLE4',
+    'TROUBLE5',
+
+    'SW2_3',
+    'W15_6',
+    'WALL40_1',
+    'WALL63_2',
+    'WALL76_1',
+    'W109_1',
+    'W109_2',
+    'W110_1',
+    'W113_1',
+    'W113_2',
+    'W113_3',
+]
+
+SIDEDEF_SWITCH_TRIPLETS = [
+    ['MAP09', 'SKY4', 'MSKY1'],
+    ['MAP10', 'DBRAIN1', 'WATER'],
+    ['MAP10', 'SW1COMP', 'TT1COMP'],
+    ['MAP10', 'SW2COMP', 'TT2COMP'],
+    ['MAP10', 'SW1STON1', 'TT1STON1'],
+    ['MAP10', 'SW2STON1', 'TT2STON1'],
+    ['MAP11', 'SW1STON2', 'TT1STON2'],
+    ['MAP11', 'SW2STON2', 'TT2STON2'],
+    ['MAP12', 'SW1BRIK', 'TT1BRIK'],
+    ['MAP12', 'SW2BRIK', 'TT2BRIK'],
+    ['MAP12', 'SW1STON3', 'TT1STON3'],
+    ['MAP12', 'SW2STON3', 'TT2STON3'],
+    ['MAP12', 'SW1STON4', 'TT1STON4'],
+    ['MAP12', 'SW2STON4', 'TT2STON4'],
+    ['MAP15', 'SW1VINE', 'TT1VINE'],
+    ['MAP15', 'SW2VINE', 'TT2VINE'],
+    ['MAP15', 'SW1PIPE', 'TT1PIPE'],
+    ['MAP15', 'SW2PIPE', 'TT2PIPE'],
+    ['MAP15', 'SW1STON5', 'TT1STON5'],
+    ['MAP15', 'SW2STON5', 'TT2STON5'],
+    ['MAP15', 'SW1STON6', 'TT1STON6'],
+    ['MAP15', 'SW2STON6', 'TT2STON6'],
+    ['MAP15', 'SW1STON7', 'TT1STON7'],
+    ['MAP15', 'SW2STON7', 'TT2STON7'],
+    ['MAP15', 'PIC00', 'PORTAL01'],
+    ['MAP15', 'PIC01', 'PORTAL02'],
+    ['MAP15', 'PIC06', 'PORTAL03'],
+    ['MAP15', 'PIC07', 'PORTAL04'],
+    ['MAP15', 'PIC13', 'PORTAL05'],
+]
+
+ALL_MAP_TRIPLETS = [
     ['MAP01', 'DANTE25.WAD', 'MAP02'],
     ['MAP02', 'ACHRON22.WAD', 'MAP03'],
+    ['MAP03', 'VIRGIL.WAD', 'MAP03'],
+    ['MAP04', 'MINOS.WAD', 'MAP05'],
+    ['MAP05', 'NESSUS.WAD', 'MAP07'],
+    ['MAP06', 'GERYON.WAD', 'MAP08'],
+    ['MAP07', 'VESPERAS.WAD', 'MAP09'],
+    ['MAP08', 'DOOM.WAD', 'E4M7'],
     ['MAP09', 'UDTWiD.wad', 'E4M8'],
 
     ['MAP10', 'MINES.WAD', 'MAP01'],
     ['MAP11', 'anomaly.wad', 'MAP01'],
     ['MAP12', 'FARSIDE.WAD', 'MAP01'],
+    ['MAP13', 'MANOR.WAD', 'MAP01'],
+    ['MAP14', 'TTRAP.WAD', 'MAP01'],
     ['MAP15', 'TROUBLE.WAD', 'MAP01'],
 
     ['MAP16', 'CABALL.WAD', 'MAP24'],
     ['MAP17', 'CABALL.WAD', 'MAP25'],
     ['MAP18', 'CABALL.WAD', 'MAP26'],
+    ['MAP19', 'BLOODSEA.WAD', 'MAP07'],
     ['MAP20', 'CABALL.WAD', 'MAP27'],
     ['MAP21', 'CABALL.WAD', 'MAP28'],
+    ['MAP22', 'BLACKTWR.WAD', 'MAP25'],
+    ['MAP23', 'MEPHISTO.WAD', 'MAP07'],
     ['MAP24', 'CABALL.WAD', 'MAP30'],
+    ['MAP25', 'TEETH.WAD', 'MAP31'],
     ['MAP26', 'CABALL.WAD', 'MAP29'],
+    ['MAP27', 'TEETH.WAD', 'MAP32'],
 
     ['MAP28', 'cpu.wad', 'MAP01'],
+    ['MAP29', 'SUBSPACE.WAD', 'MAP01'],
+    ['MAP30', 'COMBINE.WAD', 'MAP01'],
     ['MAP31', 'device_1.wad', 'MAP01'],
+    ['MAP32', 'FISTULA.WAD', 'MAP01'],
+    ['MAP33', 'SUBTERRA.WAD', 'MAP01'],
     ['MAP34', 'dmz.wad', 'MAP01'],
+    ['MAP35', 'CATWALK.WAD', 'MAP01'],
     ['MAP36', 'cdk_fury.wad', 'MAP01'],
     ['MAP38', 'e_inside.wad', 'MAP01'],
+    ['MAP37', 'GARRISON.WAD', 'MAP01'],
     ['MAP39', 'hive.wad', 'MAP01'],
 
+    ['MAP40', 'TNT.WAD', 'MAP01'],
+    ['MAP41', 'TNT.WAD', 'MAP17'],
     ['MAP42', 'TWM01.WAD', 'MAP03'],
+    ['MAP43', 'PARADOX.WAD', 'MAP01'],
+    ['MAP44', 'ATTACK.WAD', 'MAP01'],
+    ['MAP45', 'CANYON.WAD', 'MAP01'],
     ['MAP46', 'kickdm2.wad', 'MAP01'],
 ]
 
-BASE_MAPS = [
-    'MAP01', 'MAP02', 'MAP09',
-    'MAP10', 'MAP11', 'MAP12', 'MAP15',
-    'MAP16', 'MAP17', 'MAP18', 'MAP20', 'MAP21', 'MAP24', 'MAP26',
-    'MAP28', 'MAP31', 'MAP34', 'MAP36', 'MAP38', 'MAP39',
-    'MAP42', 'MAP46',
+ALL_MAPS = [
+    'MAP01', 'MAP02', 'MAP03', 'MAP04', 'MAP05', 'MAP06', 'MAP07', 'MAP08', 'MAP09',
+    'MAP10', 'MAP11', 'MAP12', 'MAP13', 'MAP14', 'MAP15',
+    'MAP16', 'MAP17', 'MAP18', 'MAP19', 'MAP20', 'MAP21', 'MAP22', 'MAP23', 'MAP24', 'MAP25', 'MAP26', 'MAP27',
+    'MAP28', 'MAP29', 'MAP30', 'MAP31', 'MAP32', 'MAP33', 'MAP34', 'MAP35', 'MAP36', 'MAP37', 'MAP38', 'MAP39',
+    'MAP40', 'MAP41', 'MAP42', 'MAP43', 'MAP44', 'MAP45', 'MAP46',
 ]
 
-ULTIMIDI = [
+
+DOOM1_MIDI = [
     'D_INTRO', 'D_INTROA', 'D_INTER', 'D_VICTOR', 'D_BUNNY',
     'D_E1M1', 'D_E1M2', 'D_E1M3', 'D_E1M4', 'D_E1M5', 'D_E1M6', 'D_E1M7', 'D_E1M8', 'D_E1M9',
     'D_E2M1', 'D_E2M2', 'D_E2M3', 'D_E2M4', 'D_E2M5', 'D_E2M6', 'D_E2M7', 'D_E2M8', 'D_E2M9',
@@ -134,12 +276,11 @@ ULTIMIDI = [
     'D_E4M1', 'D_E4M2', 'D_E4M3', 'D_E4M4', 'D_E4M5', 'D_E4M6', 'D_E4M7', 'D_E4M8', 'D_E4M9',
 ]
 
-MIDTWID = [
+DOOM2_MIDI = [
     'D_DM2TTL', 'D_DM2INT', 'D_READ_M',
-    'D_RUNNIN', 'D_STALKS', 'D_COUNTD', 'D_BETWEE', 'D_DOOM',   'D_THE_DA', 'D_SHAWN',  'D_DDTBLU',
-    'D_IN_CIT', 'D_DEAD',   'D_STLKS2', 'D_THEDA2', 'D_DOOM2',  'D_DDTBL2', 'D_RUNNI2', 'D_DEAD2',
-    'D_STLKS3', 'D_ROMERO', 'D_SHAWN2', 'D_MESSAG', 'D_COUNT2', 'D_DDTBL3', 'D_AMPIE',  'D_THEDA3',
-    'D_ADRIAN', 'D_MESSG2', 'D_ROMER2', 'D_TENSE',  'D_SHAWN3', 'D_OPENIN', 'D_EVIL',   'D_ULTIMA',
+    'D_RUNNIN', 'D_STALKS', 'D_COUNTD', 'D_BETWEE', 'D_DOOM',   'D_THE_DA', 'D_SHAWN',  'D_DDTBLU', 'D_IN_CIT', 'D_DEAD', 'D_STLKS2',
+    'D_THEDA2', 'D_DOOM2',  'D_DDTBL2', 'D_RUNNI2', 'D_DEAD2',  'D_STLKS3', 'D_ROMERO', 'D_SHAWN2', 'D_MESSAG',
+    'D_COUNT2', 'D_DDTBL3', 'D_AMPIE',  'D_THEDA3', 'D_ADRIAN', 'D_MESSG2', 'D_ROMER2', 'D_TENSE',  'D_SHAWN3', 'D_OPENIN', 'D_EVIL', 'D_ULTIMA',
 ]
 
 #
@@ -171,10 +312,10 @@ def get_wad_filename(wad_name: str) -> str | None:
 
 
 def get_wad_pre_hash(wad_name: str) -> str | None:
-    if wad_name not in SOURCE_SHA256SUM:
+    if wad_name not in SHA256_DIGEST:
         return None
 
-    return SOURCE_SHA256SUM[wad_name]
+    return SHA256_DIGEST[wad_name]
 
 
 def get_wad_hash(wad_path: str) -> str | None:
@@ -192,12 +333,12 @@ def get_wad_hash(wad_path: str) -> str | None:
 
 
 def check_wads(found_wads: list[str]):
-    for wad in SOURCE_WADS:
+    for wad in ALL_WADS:
         if get_wad_filename(wad) == None:
             log(f'  {wad.upper()} is missing.')
 
         if get_wad_hash(DIR_SOURCE + wad) != get_wad_pre_hash(wad):
-            log(f'  {wad.upper()} checksum does not match. Continuing with caution. If you see no other errors or warnings, ignore this one.')
+            log(f'  {wad.upper()} checksum does not match. Continuing Wit')
 
         found_wads.append(wad)
 
@@ -219,8 +360,9 @@ def massive_simple_sidedef_switch(map: MapEditor, initial_tx: str, desired_tx: s
 
 def base_build() -> None:
     base = WAD()
-    alpha = WAD('alpha.wad')
+    alpha = WAD(ALPHA)
 
+    doom = WAD(DIR_SOURCE + 'DOOM.WAD')
     midi1 = WAD(DIR_SOURCE + 'ultimidi.wad')
     midi2 = WAD(DIR_SOURCE + 'midtwid2.wad')
 
@@ -229,76 +371,35 @@ def base_build() -> None:
         wad = WAD(DIR_SOURCE + triple[1])
         patch = wad.patches[triple[2]]
         alpha.patches[triple[0]] = patch
+        log('    ' + triple[0])
 
     log('  Extracting maps...')
-    for triple in MAP_TRIPLETS:
+    for triple in ALL_MAP_TRIPLETS:
         wad = WAD(DIR_SOURCE + triple[1])
         map = wad.maps[triple[2]]
         alpha.maps[triple[0]] = map
+        log(f'    Pulled {triple[0]}...')
 
-    log('    Fixing MAP09...')
-    map09 = MapEditor(alpha.maps['MAP09'])
-    massive_simple_sidedef_switch(map09, 'SKY4', 'MSKY1')
-    alpha.maps['MAP09'] = map09.to_lumps()
+    log('  Fixing maps...')
+    for triplet in SIDEDEF_SWITCH_TRIPLETS:
+        map = MapEditor(alpha.maps[triplet[0]])
+        massive_simple_sidedef_switch(map, triplet[1], triplet[2])
+        alpha.maps[triplet[0]] = map.to_lumps()
 
-    log('    Fixing MAP10...')
-    map10 = MapEditor(alpha.maps['MAP10'])
-    massive_simple_sidedef_switch(map10, 'DBRAIN1', 'WATER')
-    massive_simple_sidedef_switch(map10, 'SW1COMP', 'TT1COMP')
-    massive_simple_sidedef_switch(map10, 'SW2COMP', 'TT2COMP')
-    massive_simple_sidedef_switch(map10, 'SW1STON1', 'TT1STON1')
-    massive_simple_sidedef_switch(map10, 'SW2STON1', 'TT2STON1')
-    alpha.maps['MAP10'] = map10.to_lumps()
-
-    log('    Fixing MAP11...')
-    map11 = MapEditor(alpha.maps['MAP11'])
-    massive_simple_sidedef_switch(map11, 'SW1STON2', 'TT1STON2')
-    massive_simple_sidedef_switch(map11, 'SW2STON2', 'TT2STON2')
-    alpha.maps['MAP11'] = map11.to_lumps()
-
-    log('    Fixing MAP12...')
-    map12 = MapEditor(alpha.maps['MAP12'])
-    massive_simple_sidedef_switch(map12, 'SW1BRIK', 'TT1BRIK')
-    massive_simple_sidedef_switch(map12, 'SW2BRIK', 'TT2BRIK')
-    massive_simple_sidedef_switch(map12, 'SW1STON3', 'TT1STON3')
-    massive_simple_sidedef_switch(map12, 'SW2STON3', 'TT2STON3')
-    massive_simple_sidedef_switch(map12, 'SW1STON4', 'TT1STON4')
-    massive_simple_sidedef_switch(map12, 'SW2STON4', 'TT2STON4')
-    alpha.maps['MAP12'] = map12.to_lumps()
-
-    log('    Fixing MAP15...')
-    map15 = MapEditor(alpha.maps['MAP15'])
-    massive_simple_sidedef_switch(map15, 'SW1VINE', 'TT1VINE')
-    massive_simple_sidedef_switch(map15, 'SW2VINE', 'TT2VINE')
-    massive_simple_sidedef_switch(map15, 'SW1PIPE', 'TT1PIPE')
-    massive_simple_sidedef_switch(map15, 'SW2PIPE', 'TT2PIPE')
-    massive_simple_sidedef_switch(map15, 'SW1STON5', 'TT1STON5')
-    massive_simple_sidedef_switch(map15, 'SW2STON5', 'TT2STON5')
-    massive_simple_sidedef_switch(map15, 'SW1STON6', 'TT1STON6')
-    massive_simple_sidedef_switch(map15, 'SW2STON6', 'TT2STON6')
-    massive_simple_sidedef_switch(map15, 'SW1STON7', 'TT1STON7')
-    massive_simple_sidedef_switch(map15, 'SW2STON7', 'TT2STON7')
-    massive_simple_sidedef_switch(map15, 'PIC00', 'PORTAL1')
-    massive_simple_sidedef_switch(map15, 'PIC01', 'PORTAL2')
-    massive_simple_sidedef_switch(map15, 'PIC06', 'PORTAL3')
-    massive_simple_sidedef_switch(map15, 'PIC07', 'PORTAL4')
-    massive_simple_sidedef_switch(map15, 'PIC13', 'PORTAL5')
-    alpha.maps['MAP15'] = map15.to_lumps()
-
-    log('  Transferring lumps...')
+    log('  Organizing lumps...')
     base.data += alpha.data
     base.txdefs += alpha.txdefs
     base.patches += alpha.patches
     base.sprites += alpha.sprites
     base.graphics += alpha.graphics
 
-    for midi in ULTIMIDI:
+    for midi in DOOM1_MIDI:
         base.music[midi] = midi1.music[midi]
 
-    for midi in MIDTWID:
+    for midi in DOOM2_MIDI:
         base.music[midi] = midi2.music[midi]
 
-    for map in BASE_MAPS:
+    for map in ALL_MAPS:
         base.maps[map] = alpha.maps[map]
 
     log('  Creating base.wad...')
@@ -310,7 +411,7 @@ def main() -> None:
     found_wads: list[str] = []
     check_wads(found_wads)
 
-    if sorted(found_wads) != sorted(SOURCE_WADS):
+    if sorted(found_wads) != sorted(ALL_WADS):
         log('Error: Did not find all base wads. Check base.log for more details.')
         log('Build failed. Exiting...')
         exit(1)
