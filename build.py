e684d1601f0bdfbe44c59a6e985688e41ec6ede8
Guilherme M. Miranda <alchemist.software@proton.me>
2024-05-13 16:20:19 -0300

    updated build script

---
+++ build.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: Copyright 2024 Guilherme M. Miranda <alchemist.software@proton.me>
+#
+# Description:
+#     Simplest build script possible
+
+from os import remove
+from shutil import move
+from zipfile import ZipFile
+
+version = "v0.4.1"
+windows_pkg = f"masterpack-{version}-windows.zip"
+linux_pkg = f"masterpack-{version}-windows.zip"
+pkgs = "pkgs/"
+
+
+def create_zip_package(name: str, files: list[tuple[str, str | None]]) -> None:
+    with ZipFile(name, "w") as zipf:
+        for src_file, arc_name in files:
+            zipf.write(src_file, arc_name)
+
+
+create_zip_package(
+    windows_pkg,
+    files=[
+        ("source/delete_me.txt", None),
+        ("patches/masterpack_ml25amp.wad", None),
+        ("dist/masterpack.exe", "masterpack.exe"),
+    ],
+)
+move(windows_pkg, pkgs + windows_pkg)
+print("Windows package built")
