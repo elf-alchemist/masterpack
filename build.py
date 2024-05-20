25b7f105df126bfd18af74e95f7ab08560af924b
Guilherme M. Miranda <alchemist.software@proton.me>
2024-05-20 12:59:38 -0300

    build script now bundles addons

--- build.py
+++ build.py
@@ -15,9 +15,12 @@ version = 'v0.5.0'
 
 windows = 'masterpack-' + version + '-windows.zip'
 linux = 'masterpack-' + version + '-linux.zip'
+addons = 'masterpack-addons-' + version + '.zip'
 
 pkg_windows = 'pkgs/' + windows
 pkg_linux = 'pkgs/' + linux
+pkg_addons = 'pkgs/' + addons
+
 
 windows_cmd = 'pyinstaller src/masterpack.py ' \
     + ' --log-level WARN' \
@@ -43,10 +46,12 @@ linux_cmd = 'pyinstaller src/masterpack.py' \
     + ' --add-data=src/data.zip:.' \
     + ' --noupx'
 
+
 def create_zip_package(name: str, files: list[tuple[str, str | None]]) -> None:
     with zipfile.ZipFile(name, 'w') as zipf:
         for src_file, arc_name in files:
             zipf.write(src_file, arc_name)
+
 
 def windows_build():
     print('Building for Windows')
@@ -59,7 +64,18 @@ def windows_build():
         ],
     )
     shutil.move(windows, pkg_windows)
+    print('Bundling addons...')
+    create_zip_package(
+        addons,
+        files=[
+            ('addons/masterpack-ml25amp.wad', 'masterpack-ml25amp.wad'),
+            ('addons/masterpack-freedoom-tc.wad', 'masterpack-freedoom-tc-alpha1.wad'),
+            ('addons/masterpack-psx-tc.wad', 'masterpack-psx-tc-alpha1.wad'),
+        ]
+    )
+    shutil.move(addons, pkg_addons)
     print('Package built')
+
 
 def linux_build():
     print('Building for Windows...')
@@ -82,6 +98,16 @@ def linux_build():
         ],
     )
     shutil.move(linux, pkg_linux)
+    print('Bundling addons...')
+    create_zip_package(
+        addons,
+        files=[
+            ('addons/masterpack-ml25amp.wad', 'masterpack-ml25amp.wad'),
+            ('addons/masterpack-freedoom-tc.wad', 'masterpack-freedoom-tc-alpha1.wad'),
+            ('addons/masterpack-psx-tc.wad', 'masterpack-psx-tc-alpha1.wad'),
+        ]
+    )
+    shutil.move(addons, pkg_addons)
     print('Packages built')
 
 
@@ -89,9 +115,11 @@ if platform.system() == '':
     print('Could not assertain the operating system. Exiting safely')
     exit(1)
 
+
 if platform.system() == 'Windows':
     print('Host is Windows.')
-    linux_build()
+    windows_build()
+
 
 if platform.system() == 'Linux':
     print('Host is Linux.')
